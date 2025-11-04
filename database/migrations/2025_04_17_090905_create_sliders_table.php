2910c27e223859829dc9a494d48a999e3bc02ccd
ahmed <ahmedgamalfathy39@gmail.com>
2025-11-04 22:51:40 +0200

    upload edited slider migration

--- database/migrations/2025_04_17_090905_create_sliders_table.php
+++ database/migrations/2025_04_17_090905_create_sliders_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Enums\isActive;
+use App\Enums\IsActive;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
